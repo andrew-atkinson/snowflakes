053d1b27e0afdc659fc25a161c48aae1ce824f28
andrew atkinson <blind.sam@gmail.com>
2023-11-24 08:22:19 -0600

    adds: looping and refactored for that, text customized through URL param.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,15 @@
-let flakes, font, textArr, pName;
+let flakes = [],
+  font,
+  textArr,
+  words,
+  dropIndexCounter = 0,
+  loopBeginning = 1,
+  dropFreq = 15,
+  loopPos = 1,
+  loopSpeed = 3,
+  newSetUp = false,
+  pauseDuration = 60,
+  dropSpeed = 0.3;
 
 function preload() {
   font = loadFont("fonts/Pacifico-Regular.ttf");
@@ -7,30 +18,33 @@ function preload() {
 function setup() {
   createCanvas(windowWidth - 10, windowHeight - 10);
   noStroke();
-
-  let str = getURLParams();
-  if (str.name) {
-    pName = str.name.replace(/%20/g, " ");
-    textArr = font.textToPoints(pName, 200, height / 2, 100, {
-      sampleFactor: 0.25,
-    });
-  }
-
-  flakes = [];
-  for (let i = 0; i < 180; i++) {
-    let flake = new Flake();
-    flake.pointsArr = flake.createPoints();
-    flakes.push(flake);
-  }
+  textArr = setUpText();
+  flakes = makeFlakes();
 }
 
 function draw() {
   background(20);
   stroke(255);
   strokeWeight(1);
-
+  loopPos += loopSpeed;
   if (textArr) {
-    textArr.forEach((p) => point(p.x, p.y));
+    if (loopPos <= textArr.length) {
+      // draw the text
+      drawPoints(loopPos, textArr);
+    } else if (loopPos < textArr.length + pauseDuration) {
+      // 'pause' with the text drawn
+      drawPoints(textArr.length, textArr);
+    } else if (loopPos >= textArr.length + pauseDuration && !newSetUp) {
+      // draw the points falling
+      fallPoints(textArr, dropIndexCounter);
+      dropIndexCounter += dropFreq;
+    } else {
+      // start the loop again and reset variables
+      textArr = setUpText();
+      loopPos = 0;
+      dropIndexCounter = 0;
+      newSetUp = false;
+    }
   }
 
   for (let i = 0; i < flakes.length; i++) {
@@ -71,20 +85,88 @@ class Flake {
 
   draw() {
     push();
-      translate(this.x, this.y);
-      rotate(this.a + this.spin);
-      for (let i = 0; i < 6; i++) {
-        for (let j = 0; j < this.pointsArr.length; j++) {
-          this.pointsArr[j].c.setAlpha(
-            random(this.pointsArr[j].s, this.pointsArr[j].s * 65)
-          );
-          stroke(this.pointsArr[j].c);
-          strokeWeight(this.pointsArr[j].s);
-          point(this.pointsArr[j].x, this.pointsArr[j].y);
-        }
-        rotate(PI / 3);
+    translate(this.x, this.y);
+    rotate(this.a + this.spin);
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < this.pointsArr.length; j++) {
+        this.pointsArr[j].c.setAlpha(
+          random(this.pointsArr[j].s, this.pointsArr[j].s * 65)
+        );
+        stroke(this.pointsArr[j].c);
+        strokeWeight(this.pointsArr[j].s);
+        point(this.pointsArr[j].x, this.pointsArr[j].y);
       }
+      rotate(PI / 3);
+    }
     pop();
     this.a += this.spin;
   }
+}
+
+function makeFlakes() {
+  let arr = [];
+  for (let i = 0; i < 180; i++) {
+    let flake = new Flake();
+    flake.pointsArr = flake.createPoints();
+    arr.push(flake);
+  }
+  return arr;
+}
+
+function drawPoints(loopPos, points) {
+  for (let i = 0; i < loopPos; i++) {
+    strokeWeight(random(1, 5));
+    point(points[i].x, points[i].y);
+  }
+}
+
+function fallPoints(points, dropIndexCounter) {
+  let restart = true;
+  let minY = height;
+  points.forEach((p) => {
+    if (p.y < height) {
+      restart = false;
+    }
+    if (dropIndexCounter > p.dropIndex) {
+      p.speed += dropSpeed;
+      p.y += p.speed;
+    }
+    if (p.y < minY) {
+      minY = p.y;
+    }
+  });
+  drawPoints(textArr.length, points);
+  if (restart) {
+    newSetUp = true;
+  }
+}
+
+function setUpText() {
+  let textArr = ["what?"];
+  loopPos = 1;
+  let str = getURLParams();
+  if (str.name) {
+    let wordsSize = 1;
+    words = str.name.replace(/%20/g, " ");
+    textAlign(CENTER, CENTER);
+    textSize(wordsSize);
+    while (textWidth(words) + width * 0.2 <= width) {
+      textSize(wordsSize++);
+    }
+    let offset = width - textWidth(words);
+    textArr = font.textToPoints(
+      words,
+      offset / 2,
+      height / 2 + wordsSize / 3,
+      wordsSize,
+      {
+        sampleFactor: 0.15,
+      }
+    );
+    textArr.forEach((el) => {
+      el.dropIndex = floor(random(textArr.length));
+      el.speed = 1;
+    });
+  }
+  return textArr;
 }
